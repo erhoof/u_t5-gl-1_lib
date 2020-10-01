56b5f325390c4892112d38de7bf64e7641bd64c2
Pavel Bibichenko <erhoof@outlook.com>
2020-10-01 19:44:45 +0300

    Core of lib

---
+++ eGL.cpp
@@ -0,0 +1,17 @@
+// eGL.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+#include "eGL.h"
+
+// TODO: This is an example of a library function
+
+namespace eGL
+{
+	int returnOne()
+	{
+		return 1;
+	}
+}
